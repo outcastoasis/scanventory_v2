f5a77270cd933ce22d02dc7c830df2a4ab183e63
Jascha Bucher <jascha.bucher@rebsamen.net>
2025-12-01 11:55:18 +0100

    api Datei angepasst - doppelte api route

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@ const host = window.location.hostname;
 
 // Produktiv: immer den aktuellen Host nutzen
 const API_BASE = import.meta.env.PROD
-  ? `http://${host}/api`
+  ? `http://${host}`
   : import.meta.env.VITE_API_URL || "http://localhost:5050/api";
 
 export default API_BASE;
